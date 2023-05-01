3612d47a7567ef2434a3c6d3dd8b49cd5ad18449
Buchi <48419128+Aja-buchi@users.noreply.github.com>
2023-05-01 21:07:44 +0100

    code

---
+++ src/main/java/com/bujubanton/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,13 @@
+package com.bujubanton.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
